e04d7b8f67e032ea62abcfa726940952fd4fc6f8
VELONTSARA <bernadovelontsara@gmail.com>
2026-03-21 01:49:05 +0300

    Sauv avant deploiement 11

--- src/main/kotlin/DatabaseManager.kt
+++ src/main/kotlin/DatabaseManager.kt
@@ -1,27 +1,19 @@
 import java.sql.Connection
 import java.sql.DriverManager
-import java.net.URI
 
 object DatabaseManager {
     fun getConnection(): Connection? {
-        val dbUrl = System.getenv("DATABASE_URL")
+        // On récupère les 3 infos séparément
+        val url = System.getenv("DB_URL")
+        val user = System.getenv("DB_USER")
+        val pass = System.getenv("DB_PASSWORD")
 
         return try {
-            // Chargement du driver PostgreSQL
             Class.forName("org.postgresql.Driver")
 
-            if (dbUrl != null) {
-                // Utilisation de la classe URI pour découper l'URL de Render proprement
-                // Render donne : postgresql://user:password@hostname:port/dbname
-                val dbUri = URI(dbUrl)
-
-                val username = dbUri.userInfo.split(":")[0]
-                val password = dbUri.userInfo.split(":")[1]
-
-                // On reconstruit l'URL au format JDBC : jdbc:postgresql://hostname:port/dbname
-                val jdbcUrl = "jdbc:postgresql://" + dbUri.host + ":" + dbUri.port + dbUri.path
-
-                DriverManager.getConnection(jdbcUrl, username, password)
+            if (url != null && user != null && pass != null) {
+                // Connexion Render (Paramètres séparés = Zéro erreur de parsing)
+                DriverManager.getConnection(url, user, pass)
             } else {
                 // Connexion locale pour ton ASUS
                 DriverManager.getConnection(
